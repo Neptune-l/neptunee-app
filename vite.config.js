ab5c2e6cf1fa56198174dc6f6c652f1bfa35068b
Neptune <neptune@users.noreply.github.com>
2026-07-30 19:55:04 +0800

    Update base path for new repo neptunee-app

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/neptune-app/',
+  base: '/neptunee-app/',
   plugins: [
     react(),
     VitePWA({
